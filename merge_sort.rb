91960b9455df07101c71b6bc83fd0f1122f03ffc
Kasper <kasperbak89@pm.me>
2022-12-01 12:23:28 +0100

    Add pseudo code for solving the problem

---
+++ merge_sort.rb
@@ -0,0 +1,15 @@
+
+#Build a method #merge_sort that takes in an array and returns a sorted array,
+# using a recursive merge sort methodology.
+# Tips:
+# Think about what the base case is and what behavior is happening again and again
+# and can actually be delegated to someone else (e.g. that same method!).
+# It may be helpful to check out the background videos again if you don’t quite
+# understand what should be going on.
+
+
+# Pseudo code for recursive merge_sort method:
+# Given an array divide that array in 2 pieces as even as possible
+# Sort the left piece, aka call the recursive method on the left piece
+# Sort the right piece, aka call the recursive method on the right piece
+# Merge the two pieces in an ordered way
