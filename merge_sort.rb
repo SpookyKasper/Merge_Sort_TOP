e37d0a2dfc0935e18a91a649bc19cbe9c43d1c2f
Kasper <kasperbak89@pm.me>
2022-12-08 15:57:18 +0100

    Implement a functioning merge sort method (youpi!)

--- merge_sort.rb
+++ merge_sort.rb
@@ -27,43 +27,37 @@
 
 my_array = [4, 5, 7, 1, 3, 6, 2]
 my_simples_array = [2, 1, 3]
-one_value_arr = [2]
 
-puts "This is the value return by the attempt to split an array of 1 item in a left_half #{one_value_arr.take(one_value_arr.length/2)}"
-puts "This is the value returned by the attempt to split an array of 1 item in a right_half #{one_value_arr.drop(one_value_arr.length/2)}"
+def merge_sort(array, sorted=[], round=0)
+  return array if array.length == 1
 
-def merge(left, right)
-end
-
-# Merg algo:
-#
-
-
-def merge_sort(array, sorted=[])
-  if array.length == 1
-    puts "#{array} is sorted, ready for merge"
-    return array
-  end
   left_half = array.take(array.length/2)
-  p "This is the left half #{left_half}"
   right_half = array.drop(array.length/2)
-  p "This is the righ half #{right_half}"
-  sorted_left = merge_sort(left_half, sorted)
-  sorted_right = merge_sort(right_half, sorted)
-  if sorted_left[0] > sorted_right[0]
-    sorted << sorted_right[0]
-    sorted << sorted_left[0]
-  else
-    sorted << sorted_left[0]
-    sorted << sorted_right[0]
+  p "This is the left half #{left_half}"
+  p "This is the right half #{right_half}"
+  s_left = merge_sort(left_half, sorted)
+  s_right = merge_sort(right_half, sorted)
+  p "This is the sorted left #{s_left}"
+  p "This is the sorted right #{s_right}"
+  sorted = []
+  until s_left.empty? || s_right.empty?
+    if s_left[0] < s_right[0]
+      puts "left value #{s_left[0]} goes"
+      sorted << s_left.shift(1)
+    else
+      puts "right value #{s_right[0]} goes"
+      sorted << s_right.shift(1)
+    end
   end
-  p "This is the sorted array #{sorted}"
-  p "This is the sorted_left #{sorted_left}"
-  p "This is the sorted_right #{sorted_right}"
-  p "This is the first item of the sorted left #{sorted_left[0]}"
-  p "This is the first item of the sorted right #{sorted_right[0]}"
-  p sorted_right
+  if s_left.empty?
+    s_right.each { |el| sorted << el }
+  end
+  if s_right.empty?
+    s_left.each { |el| sorted << el }
+  end
+  p "This is the soted array #{sorted.flatten}"
+  sorted.flatten
 end
 
 
-merge_sort(my_simples_array)
+merge_sort(my_array)
